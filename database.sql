97218d14c366fd34b00d6e253dfa57191fbcc1c1
Gabriel Barros <silvagabr57@gmail.com>
2026-06-02 22:10:34 -0300

    database.sql added

---
+++ database.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (name, email, password) VALUES ('João Silva', 'joao@example.com', 'senha123');
+INSERT INTO users (name, email, password) VALUES ('Maria Souza', 'maria@example.com', 'senha456');
